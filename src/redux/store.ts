dda88c66998a52ad2ffc4b7c18a28510893bfd9e
AlFilip <t9371100211@gmail.com>
2021-12-12 20:24:02 +0300

    edited eslint, added thunkType

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,16 +1,17 @@
-import { applyMiddleware, combineReducers, createStore } from 'redux'
-
-import thunk from 'redux-thunk'
-
-import auth from './authReducer'
-
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import auth from './authReducer';
 
 
 const rootReducer = combineReducers( {
-    auth,
-} )
+	auth,
+} );
 
-export const store = createStore( rootReducer, applyMiddleware( thunk ) )
+export const store = createStore( rootReducer, applyMiddleware( thunk ) );
 
 //@ts-ignore
-window.store = store
+window.store = store;
+
+// type RootState = ReturnType<typeof rootReducer>
+
+// export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, AnyAction>
