740e49d108cadd6d5114e08de69546119a9a4256
Mari-marina <shunkina9292@mail.ru>
2021-12-13 23:29:35 +0300

    checkbox

--- src/assets/Components/auth/auth-login/AuthLogin.tsx
+++ src/assets/Components/auth/auth-login/AuthLogin.tsx
@@ -2,6 +2,7 @@ import s from "./AuthLogin.module.scss";
 import eye from "./../../../images/auth-img/eye.svg";
 import FormPropsTextFieldsEmail from "../../mui/text-field/TextFieldEmail";
 import FormPropsTextFieldsPass from "../../mui/text-field/TextFieldPass";
+import Checkboxes from "./../../mui/checkbox/Checkbox";
 
 function AuthLogin() {
   // const vlas = "vlas";
@@ -16,9 +17,15 @@ function AuthLogin() {
           </div>
           <img className={s.img} src={eye} alt="" />
           <FormPropsTextFieldsPass />
-          <a className={s.linkNewPass} href="#">
-            Forgot Password
-          </a>
+          <div className={s.checkboxAndFogot}>
+            <div className={s.checkbox}>
+              <Checkboxes />
+              <p className={s.checkboxText}>remember me</p>
+            </div>
+            <a className={s.linkNewPass} href="#">
+              Forgot Password
+            </a>
+          </div>
           <button className={s.btn}>Login</button>
         </form>
         <div className={s.wrapLink}>
