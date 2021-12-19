24ae26243f860148a2117fe8dbae8a9795708a6a
Mari-marina <shunkina9292@mail.ru>
2021-12-20 01:52:53 +0300

    rating

---
+++ src/assets/Components/mui/rating/Rating.tsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Rating from "@mui/material/Rating";
+
+const StyledRating = styled(Rating)({
+  "& .MuiRating-iconFilled": {
+    color: "#21268F",
+  },
+  "& .MuiRating-iconHover": {
+    color: "#21268F",
+  },
+});
+
+export default function CustomizedRating() {
+  return (
+    <Box
+      sx={{
+        "& > legend": { mt: 5 },
+      }}
+    >
+      <StyledRating name="customized-5" defaultValue={5} max={5} size="small" />
+    </Box>
+  );
+}
