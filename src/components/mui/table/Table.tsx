4aee6b242aebc6614eb5b98d7aa318e6a434c29d
Pavel Kardash <cardash@mail.ru>
2021-12-22 12:20:05 +0300

    made sorting

--- src/components/mui/table/Table.tsx
+++ src/components/mui/table/Table.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
@@ -12,10 +12,10 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import s from './Table.module.scss';
 import { useAppSelector } from '../../../redux/store';
-import { deletePackTC, packType } from '../../../redux/packs-reducer';
+import { deletePackTC, getPacks, packType } from '../../../redux/packs-reducer';
 
 
-const StyledTableRow = styled( TableRow )( ({ theme }) => ( {
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
 	'&:nth-of-type(odd)': {
 		backgroundColor: theme.palette.action.hover,
 	},
@@ -26,40 +26,79 @@ const StyledTableRow = styled( TableRow )( ({ theme }) => ( {
 	'& td': {
 		padding: 16,
 	},
-} ) );
+}));
 
 export default function DenseTable() {
-	const packs = useAppSelector<packType[]>( state => state.packsReducer.cardPacks );
+	const packs = useAppSelector<packType[]>(state => state.packsReducer.cardPacks);
 	const dispatch = useDispatch();
 	const { currentUserId } = useParams();
 
-	const rows = packs.map( m => (
-		<StyledTableRow key={ m._id }
-						sx={ { '&:last-child td, &:last-child th': { border: 0 } } }>
-			<TableCell component="th" scope="row">{ m.name }</TableCell>
-			<TableCell align="right">{ m.cardsCount }</TableCell>
-			<TableCell align="right">{ m.updated }</TableCell>
-			<TableCell align="right">{ m.created }</TableCell>
-			<TableCell align="right">
+	const [sortName, setSortName] = useState<string>('0name');
+	const [sortUpdated, setSortUpdated] = useState<string>('0updated');
+	const [sortCreated, setSortCreated] = useState<string>('0created');
+
+
+	const rows = packs.map(m => (
+		<StyledTableRow key={m._id}
+										sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+			<TableCell component='th' scope='row'>{m.name}</TableCell>
+			<TableCell align='right'>{m.cardsCount}</TableCell>
+			<TableCell align='right'>{m.updated}</TableCell>
+			<TableCell align='right'>{m.created}</TableCell>
+			<TableCell align='right'>
 				<span>update</span>
-				<span onClick={ () => dispatch( deletePackTC( m._id, currentUserId) ) }>delete</span>
+				<span onClick={() => dispatch(deletePackTC(m._id, currentUserId))}>delete</span>
 			</TableCell>
-		</StyledTableRow> ) );
+		</StyledTableRow>));
+
+	const onSortNameClickHandler = () => {
+		if (sortName === '0name') {
+			setSortName('1name');
+		}
+		if (sortName === '1name') {
+			setSortName('0name');
+		}
+		dispatch(getPacks({ user_id: currentUserId, sortPacks: sortName }));
+	};
+
+	const onSortUpdatedClickHandler = () => {
+		if (sortUpdated === '0updated') {
+			setSortUpdated('1updated');
+		}
+		if (sortUpdated === '1updated') {
+			setSortUpdated('0updated');
+		}
+		dispatch(getPacks({ user_id: currentUserId, sortPacks: sortUpdated }));
+	};
+
+	const onSortCreatedClickHandler = () => {
+		if (sortCreated === '0created') {
+			setSortCreated('1created');
+		}
+		if (sortCreated === '1created') {
+			setSortCreated('0created');
+		}
+		dispatch(getPacks({ user_id: currentUserId, sortPacks: sortCreated }));
+
+	};
+
 
 	return (
-		<TableContainer component={ Paper }>
-			<Table sx={ { minWidth: 650 } } size="small" aria-label="a dense table">
-				<TableHead className={ s.tableHead }>
+		<TableContainer component={Paper}>
+			<Table sx={{ minWidth: 650 }} size='small' aria-label='a dense table'>
+				<TableHead className={s.tableHead}>
 					<StyledTableRow>
-						<TableCell>Name</TableCell>
-						<TableCell align="right">Cards</TableCell>
-						<TableCell align="right">Last Updated</TableCell>
-						<TableCell align="right">Created by</TableCell>
-						<TableCell align="right">Actions</TableCell>
+						<TableCell onClick={onSortNameClickHandler}>Name</TableCell>
+						<TableCell align='right'>Cards</TableCell>
+						<TableCell onClick={onSortUpdatedClickHandler} align='right'>Last
+							Updated</TableCell>
+						<TableCell onClick={onSortCreatedClickHandler} align='right'>Created
+							by</TableCell>
+						<TableCell align='right'>Actions</TableCell>
 					</StyledTableRow>
 				</TableHead>
-				<TableBody className={ s.tableBody }>
-					{ rows }
+				<TableBody className={s.tableBody}>
+					{rows}
 				</TableBody>
 			</Table>
 		</TableContainer>
