94a5d648650aa402e981a2eeeaafdb4c810e2904
Pavel Kardash <cardash@mail.ru>
2021-12-24 14:37:50 +0300

    done fix

--- src/components/main/packs-list/Profile/Profile.tsx
+++ src/components/main/packs-list/Profile/Profile.tsx
@@ -6,7 +6,11 @@ import s from './Profile.module.scss';
 import RangeSlider from '../../../mui/range-slider/RangeSlider';
 import { DenseTable } from '../../../mui/table/Table';
 import { useAppSelector } from '../../../../redux/store';
-import { selectLoginData, selectPacksPageNumber, selectPacksPageSize } from '../../../../assets/selectors/authSelectors';
+import {
+	selectLoginData,
+	selectPacksPageNumber,
+	selectPacksPageSize,
+} from '../../../../assets/selectors/authSelectors';
 import { createPack, getPacks } from '../../../../redux/packs-reducer';
 import { PaginationPacksContainer } from '../../../mui/pagination/PaginationPacksContainer';
 
@@ -16,94 +20,96 @@ const defaultAva = 'https://via.placeholder.com/150';
 // import ButtonForTable from "./../../../Components/common/button/ButtonForTable";
 
 function Profile() {
-	const { name, avatar, _id } = useAppSelector( selectLoginData );
+	const { name, avatar, _id } = useAppSelector(selectLoginData);
 	const { currentUserId } = useParams();
 	const dispatch = useDispatch();
-	const page = useAppSelector( selectPacksPageNumber );
-	const pageCount = useAppSelector( selectPacksPageSize );
+	const page = useAppSelector(selectPacksPageNumber);
+	const pageCount = useAppSelector(selectPacksPageSize);
 
-	const [addPackValue, setAddPackValue] = useState( '' );
-	const [search, setSearch] = useState<string>( '' );
+	const [addPackValue, setAddPackValue] = useState('');
+	const [search, setSearch] = useState<string>('');
 
 
-	useEffect( () => {
+	useEffect(() => {
 		_id &&
 		dispatch(
-			getPacks( {
+			getPacks({
 				user_id: currentUserId ? currentUserId : _id,
 				page,
 				pageCount,
-			} ),
+			}),
 		);
-	}, [currentUserId, page, pageCount] );
-
-
+	}, [currentUserId, page, pageCount]);
 
 
 	const addNewPackClickHandler = (e: FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
-		addPackValue && dispatch( createPack( addPackValue, currentUserId ) );
-		setAddPackValue( '' );
+		addPackValue && dispatch(createPack(addPackValue, currentUserId));
+		setAddPackValue('');
 	};
 	const searching = () => {
-		dispatch( getPacks( {user_id: currentUserId,  packName: search } ) );
+		if (_id) {
+			dispatch(getPacks({
+				user_id: currentUserId ? currentUserId : _id,
+				packName: search,
+			}));
+
+		}
 	};
 
 	const handlerSearch = (e: ChangeEvent<HTMLInputElement>) => {
-		setSearch( e.currentTarget.value );
+		setSearch(e.currentTarget.value);
 	};
-
-
 
 
 	return (
 		<div>
-			<div className={ s.wrapper }>
-				<div className={ s.wrapLeft }>
-					<div className={ s.wrapPerson }>
-						<img className={ s.img } src={ avatar ? avatar : defaultAva } alt=''/>
-						<h3 className={ s.subtitle }>{ name }</h3>
-						{ _id === currentUserId ||
-						( !currentUserId && (
-							<Link to={ '/edit-profile' }>
-								<button className={ s.btnEditProfile }>Edit Profile</button>
+			<div className={s.wrapper}>
+				<div className={s.wrapLeft}>
+					<div className={s.wrapPerson}>
+						<img className={s.img} src={avatar ? avatar : defaultAva} alt='' />
+						<h3 className={s.subtitle}>{name}</h3>
+						{_id === currentUserId ||
+						(!currentUserId && (
+							<Link to={'/edit-profile'}>
+								<button className={s.btnEditProfile}>Edit Profile</button>
 							</Link>
-						) ) }
+						))}
 					</div>
-					<div className={ s.wrapSlider }>
-						<h3 className={ s.subtitleSlid }>Number of cards</h3>
-						<RangeSlider/>
+					<div className={s.wrapSlider}>
+						<h3 className={s.subtitleSlid}>Number of cards</h3>
+						<RangeSlider />
 					</div>
 				</div>
-				<div className={ s.wrapRight }>
-					<h2 className={ s.title }>{ `${ name } Packs list` }</h2>
-					<div className={ s.wrapForm }>
-						<form className={ s.wrapForm } onSubmit={ addNewPackClickHandler }>
-						<input
-							className={ s.input }
-							value={ addPackValue }
-							onChange={ (e) => setAddPackValue( e.currentTarget.value ) }
-							placeholder='Type here'
-						/>
-						<button className={ s.button }>Add new pack</button>
-					</form>
-					<div className={ s.wrapForm }>
-						<input
-							className={ s.input }
-							value={ search }
-							onChange={ handlerSearch }
-							type='text'
-							placeholder='Search...'
-						/>
-						<button className={ s.button } onClick={ searching }>
-							Search
-						</button>
+				<div className={s.wrapRight}>
+					<h2 className={s.title}>{`${name} Packs list`}</h2>
+					<div className={s.wrapForm}>
+						<form className={s.wrapForm} onSubmit={addNewPackClickHandler}>
+							<input
+								className={s.input}
+								value={addPackValue}
+								onChange={(e) => setAddPackValue(e.currentTarget.value)}
+								placeholder='Type here'
+							/>
+							<button className={s.button}>Add new pack</button>
+						</form>
+						<div className={s.wrapForm}>
+							<input
+								className={s.input}
+								value={search}
+								onChange={handlerSearch}
+								type='text'
+								placeholder='Search...'
+							/>
+							<button className={s.button} onClick={searching}>
+								Search
+							</button>
+						</div>
 					</div>
-					</div>
-					<div className={ s.table }>
-						<DenseTable/>
-						<div className={ s.wrapBottom }>
-							<PaginationPacksContainer/>
+					<div className={s.table}>
+						<DenseTable />
+						<div className={s.wrapBottom}>
+							<PaginationPacksContainer />
 						</div>
 					</div>
 				</div>
